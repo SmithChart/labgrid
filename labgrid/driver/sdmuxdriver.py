80cd5fd9cd3a4f82bfe8bab8eb5fbde60e551bef
Chris Fiege <c.fiege@pengutronix.de>
2017-12-07 08:19:09 +0100

    driver: modified sdmuxdriver to work with NetworkSdmuxResource
    Signed-off-by: Chris Fiege <c.fiege@pengutronix.de>

--- labgrid/driver/sdmuxdriver.py
+++ labgrid/driver/sdmuxdriver.py
@@ -1,20 +1,42 @@
 import attr
+import subprocess
+import logging
 
 from labgrid.factory import target_factory
 from labgrid.driver.common import Driver
-from labgrid.protocol import SdmuxProtocoll
-from labgrid.resource import SdmuxPort
+from labgrid.protocol import SdmuxProtocol
+from labgrid.resource import SdmuxPort, NetworkSdmuxPort
 
 @target_factory.reg_driver
 @attr.s(cmp=False)
-class SdmuxDriver(Driver, SdmuxProtocoll):
-    bindings = {"sg": SdmuxPort }
+class SdmuxDriver(Driver, SdmuxProtocol):
+    bindings = {"sdmuxport": {SdmuxPort, NetworkSdmuxPort} }
 
+    def __attrs_post_init__(self):
+        super().__attrs_post_init__()
+        self.logger = logging.getLogger("{}({})".format(self, self.target))
+
+    @Driver.check_active
     def off(self):
-        print("Switching sdmux off")
+        self._exec("off")
 
+    @Driver.check_active
     def host(self):
-        print("Switching to host")
+        self._exec("host")
 
+    @Driver.check_active
     def dut(self):
-        print("Switching to DUT")
+        self._exec("dut")
+
+    @Driver.check_active
+    def _exec(self, target):
+        print("Switching sdmux {} to: {}".format(self.sdmuxport.sg, target))
+        self.logger.debug("Switching sdmux {} to: off".format(self.sdmuxport.sg))
+
+        args = "usbsdmux -c {} off".format(self.sdmuxport.sg).split(" ")
+
+        if isinstance(self.sdmuxport, SdmuxPort):
+            proc = subprocess.check_call(args)
+        elif isinstance(self.sdmuxport, NetworkSdmuxPort):
+            proc = subprocess.check_call(self.sdmuxport.command_prefix + args)
+
